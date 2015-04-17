397e8c0e96f05bad938bbc1a98ba2b7992563921
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-04-16 23:18:33 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+webpack-example
+===============
+
+### How to use
+
+- `npm install`
+- `bower install`
+- `npm run dev` - localhost:8080
+- `npm run deploy` - deploy to dist folder
---
+++ app/main.js
@@ -0,0 +1,10 @@
+require('./main.css');
+
+var React = require('react');
+var Home = require('./home/Home.js');
+
+React.render(Home(), document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept();
+}
