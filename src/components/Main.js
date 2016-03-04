1d1dddcd9a383d9eb4e3a52c7d12c09f7ba1b998
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-03-03 20:15:27 -0800

    Migrate files to src/

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>Hello World.</h1>
+      </section>
+    );
+  }
+}
+
+export default AppComponent;
